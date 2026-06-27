9014c3d82a8bb588f6e4214c231a24cd96266550
RicardoGS1 <granadosacerioricardo@gmail.com>
2026-06-27 23:48:10 +0200

    bump version to 1.3.1
    - update `versionCode` to 100300100
    - update `versionName` to "1.3.1" in `app/build.gradle.kts`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.virtualworld.easyexpensecontrol"
         minSdk = 24
         targetSdk = 35
-        versionCode = 100200900
-        versionName = "1.2.9"
+        versionCode = 100300100
+        versionName = "1.3.1"
 
         buildConfigField("String", "GEMINI_API_KEY", "\"$geminiKey\"")
 
